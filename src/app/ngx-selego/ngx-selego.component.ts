52114b944d3371aeb6a948967df103ae5d2e794d
danilo@danilo-PC <danilo@danilo-PC>
2018-01-30 17:10:39 -0500

    Se comienza a realizar controlvalueaccessor

--- src/app/ngx-selego/ngx-selego.component.ts
+++ src/app/ngx-selego/ngx-selego.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'ngx-selego',
   templateUrl: './ngx-selego.component.html',
-  styleUrls: ['./ngx-selego.component.css']
+  styleUrls: ['./ngx-selego.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NgxSelegoComponent,
+    multi: true
+  }]
 })
-export class NgxSelegoComponent implements OnInit {
+
+export class NgxSelegoComponent implements OnInit, ControlValueAccessor {
+
+  private value: any;
+  private onChange: Function;
 
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  writeValue(value: any) {
+    this.value = value;
+    console.log(value);
+  }
+
+  registerOnChange(fn) { this.onChange = fn; }
+
+  registerOnTouched(fn) { }
 
 }
 
